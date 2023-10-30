10fa7e586385995c7a01d67de144911c771751d4
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:51:03 +0100

    cuarta version

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("BABOU");
+        System.out.println("Quiero irme al gym.");
+        System.out.println("Quiero irme al gym.");
+        System.out.println("Quiero irme al gym.");
+        System.out.println("Quiero irme al gym.");
+        System.out.println("Quiero irme al gym.");
+        System.out.println("Quiero irme al gym.");
+        System.out.println("Quiero irme al gym.");
         System.out.println("Quiero irme al gym.");
 
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
